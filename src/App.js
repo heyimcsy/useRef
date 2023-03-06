dff01db53c2e374e5debbc3e45d2c944c65f6236
최서윤 <cinnamon1995@naver.com>
2023-03-06 22:24:52 +0900

    10개 치면 ㅍㅗ커싱이동

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 
 function App() {
   const idRef = useRef('')
+  const pwRef = useRef('')
+
+  const [id, setId] = useState('')
   //화면이 렌더링 될 때, 어떤 작업을 하고 싳싶다 : useEfeect!!
 
   useEffect(() => {
     idRef.current.focus()
   }, [])
 
+  useEffect(() => {
+    if (id.length >= 10) {
+      pwRef.current.focus()
+    }
+  }, [id])
+
   return (
     <>
       <div>
-        아이디 : <input type="text" />
+        아이디 :{' '}
+        <input
+          valu={id}
+          onChange={(event) => {
+            setId(event.target.value)
+          }}
+          type="text"
+          ref={idRef}
+        />
       </div>
       <div>
-        비밀번호 : <input type="password" ref={idRef} />
+        비밀번호 : <input type="password" ref={pwRef} />
       </div>
     </>
   )
